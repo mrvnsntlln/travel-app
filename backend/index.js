8b67d208184fcfde56b753f5b4a3e4c61085d676
marvin santillan <mrvnsntlln10@gmail.com>
2022-08-15 16:23:27 +0800

    Host

--- backend/index.js
+++ backend/index.js
@@ -4,10 +4,13 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const userRoute = require("./routes/users");
 const pinRoute = require("./routes/pins");
+const cors = require('cors')
+const path = require('path')
 
 dotenv.config();
 
 app.use(express.json());
+app.use(cors())
 
 mongoose 
  .connect(process.env.MONGO_URL, {
@@ -20,6 +23,13 @@ mongoose
 app.use("/api/users", userRoute);
 app.use("/api/pins", pinRoute);
 
-app.listen(8000, () => {
+if (process.env.NODE_ENV === "production"){
+  app.use(express.static("frontend/build"));
+  app.get("*",(req,res)=>{
+    res.sendFile(path.resolve(__dirname,"frontend","build","App.js"))
+  })
+}
+
+app.listen(process.env.PORT || 8000, () => {
   console.log("Backend server is running!");
 });
